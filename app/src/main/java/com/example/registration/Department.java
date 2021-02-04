83ca73afd15b3a3690653de2f0e1caae7871ac57
ShaishavMaisuria <smaisuri@uncc.edu>
2021-02-03 20:50:34 -0500

    select main works

--- app/src/main/java/com/example/registration/Department.java
+++ app/src/main/java/com/example/registration/Department.java
@@ -2,13 +2,51 @@ package com.example.registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class Department extends AppCompatActivity {
 
+    String checkValue;
+    final static public String Name_key ="Department Name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_department);
+
+        findViewById(R.id.buttonCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radioButtonComputerScience){
+                    checkValue=getResources().getString(R.string.StringcomputerScience);
+                }
+            }
+        });
+
+
+        findViewById(R.id.buttonSelectDepartment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent department_Intent= new Intent();
+
+                department_Intent.putExtra(Name_key,checkValue);
+                setResult(RESULT_OK,department_Intent);
+                Log.d("demo","departmentSelect");
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/registration/MainActivity.java
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.registration;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView department_value;
+    final static public int REQ_CODE =100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +20,47 @@ public class MainActivity extends AppCompatActivity {
 
         TextView name=findViewById(R.id.textViewName);
 
+        department_value=findViewById(R.id.textViewDepartmentValue);
+
+        findViewById(R.id.buttonSelect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this,Department.class);
+
+                startActivityForResult(intent,REQ_CODE); //s
+            }
+        });
+
+
+
+
+
+//        if(getIntent()!=null && getIntent().getExtras()!=null && getIntent().hasExtra(Department.Name_key)){
+//            String department =getIntent().getStringExtra(Department.Name_key);
+//            department_value.setText(String.valueOf(department));
+//            Log.d("demo","Mainactivity intent");
+//        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("demo","OnactivityResult "+data.toString());
+        if(resultCode==RESULT_OK ) {
+            Log.d("demo", "OnactivityResult OKAY ");
+
+            if(data!=null && data.hasExtra(Department.Name_key)){
+
+                String dept_data=data.getStringExtra(Department.Name_key);
+                Log.d("demo", "data "+dept_data);
+
+                department_value.setText(String.valueOf(dept_data));
+
+            }
+        }
+        else if(resultCode==RESULT_CANCELED){
+            Log.d("demo","OnactivityResult NOT OKAY");
+        }
     }
 }
